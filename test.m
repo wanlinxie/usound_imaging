e16123ec0384f43b7b84c3a873213c2fac992f84
Miguel Angel Gutierrez <mag2293@columbia.edu>
2017-08-03 17:51:59 -0400

    Housekeeping. Should work with mock data and data imported from the scope.

--- test.m
+++ test.m
@@ -1,83 +1,81 @@
 % % Generate mock data
-% mock_size = 5;
-% angle_sweep = 45;
-% [angle_count, sample_count, angles_file, intensity_file] = generate_data(mock_size, angle_sweep, 'boundary', 'gradient', 'ideal');
+% mock_size = 10;
+% angle_sweep = 55;
+% [angle_count, sample_count, angles_file, intensity_file] = generate_data(mock_size, angle_sweep, 'density', 'random', 'ideal');
+% angles = angles_file;
+% intensity = intensity_file;
+% MAX_SCAN_LEN = mock_size;
+
 
 import_scope
-
 angle_sweep = 30;
 angle_count = size(intensity, 2);
 sample_count = MAX_SCAN_LEN;
 angles = linspace(0, angle_sweep, angle_count);
 
-% Sort data
+%% Sort data
 angles = angles';
 intensity = intensity';
 [angles, intensity] = sort_data(angles, intensity);
 
-% Adjust angles for imaging
+%% Adjust angles for imaging
 disp('Adjusting Angles');
 adjusted_angles = adjust_angles(angles, angle_sweep);
 
-% Generate position information for each sample along the scan line
+%% Generate position information for each sample along the scan line
 disp('Generating Positions');
 position_matrix = pol2cart(adjusted_angles, angle_count, sample_count);
 
-% Initalie a blank grid
+%% Initalize a blank grid
 grid_resolution = 1;
-grid_width = MAX_SCAN_LEN; %ceil(max(max(abs(position_matrix(:,:,1)))));
-grid_height = MAX_SCAN_LEN;
-[grid, grid_width, grid_height] = init_grid(grid_width, grid_height, grid_resolution);
+[grid, grid_width, grid_height] = init_grid(angle_sweep, MAX_SCAN_LEN, grid_resolution);
 
-% Adjust the positions to fit the grid
+%% Adjust the positions to fit the grid
 disp('Adjusting Positions for Grid');
 position_matrix_adjusted = adjust_position(position_matrix, grid_width, grid_resolution);
 
-% Plot data points onto a grid
+%% Plot data points onto a grid
 disp('Plotting Points to Grid');
 grid1 = grid;
 plot_grid = cell(grid_height, grid_width);
 [grid1, plot_grid] = plot_to_grid(plot_grid, position_matrix_adjusted, angle_count, sample_count, intensity, grid);
-
-horiz_width = ceil(max(max(abs(position_matrix(:,:,1)))));
-horiz_range = (((grid_width/2) - horiz_width):((grid_width/2) + horiz_width));
-temp = grid1(:,horiz_range);
+image(grid1, 'CDataMapping','scaled');
 
 f1 = figure(1);
-image(temp,'CDataMapping','scaled');
-axis image;
+image(grid1,'CDataMapping','scaled');
 title('(1) SCOPE - Pre Interpolation');
-
-%%
-
-% UNDER-CONSTRUCTION Sector Averaging and Interpolation
-disp('Interpolating...');
-interpolation_grid = cell(grid_height, grid_width, 2);
-% Maximum Value = (1/2) * angle_count
-sector_span = 3;
-grid2 = grid;
-[grid2, interpolation_grid] = interpolate(interpolation_grid, position_matrix_adjusted, angle_count, sample_count, intensity, grid2, grid_width, grid_height, sector_span);
-
-temp = grid2(:,horiz_range);
-
-f2 = figure(2);
-image(temp,'CDataMapping','scaled');
-title('(2) SCOPE - Post-Interpolation');
 axis image;
 
-% Smooth image boundaries
+%% UNDER-CONSTRUCTION Sector Averaging and Interpolation
+% % This interpolation scheme works well with mock data, however it is too
+% % slow to work with real data
+% 
+% disp('Interpolating...');
+% interpolation_grid = cell(grid_height, grid_width, 2);
+% 
+% % Maximum Value = (1/2) * angle_count
+% sector_span = round(angle_count/5);
+% grid2 = grid;
+% [grid2, interpolation_grid] = interpolate(interpolation_grid, position_matrix_adjusted, angle_count, sample_count, intensity, grid2, grid_width, grid_height, sector_span);
+% 
+% f2 = figure(2);
+% image(grid2,'CDataMapping','scaled');
+% title('(2) SCOPE - Post-Interpolation');
+% axis image;
+grid2 = grid1;
+
+%% Smooth image boundaries
 disp('Smoothing Boundaries');
 grid3 = grid2;
 grid3 = boundary_smoothing(adjusted_angles, angle_count, sample_count, grid3, grid_width, grid_height, grid_resolution);
-
-temp = grid3(:,horiz_range);
+grid3 = draw_bounds(grid3, position_matrix_adjusted, angle_count, sample_count, 100);
 
 f3 = figure(3);
-image(temp, 'CDataMapping','scaled');
+image(grid3, 'CDataMapping','scaled');
 title('(3) SCOPE - Smoothing Boundaries');
 axis image;
 
-% Control image contast and color levels
+%% Control image contast and color levels
 disp('Color Levels');
 intensity_min = 0;
 intensity_max = max(max(intensity));
@@ -86,31 +84,24 @@ white_threshold = 255; % 255
 level_count = 255;
 
 grid4 = grid3;
-grid4 = draw_bounds(grid4, position_matrix_adjusted, angle_count, sample_count, white_threshold);
 grid4 = contrast(grid4, intensity_min, intensity_max, black_threshold, white_threshold, level_count);
-
-temp = grid4(:,horiz_range);
+temp = draw_bounds(grid4, position_matrix_adjusted, angle_count, sample_count, white_threshold);
 
 f4 = figure(4);
-I = imshow(temp, [0 255]);
+I = imshow(temp, [0 white_threshold]);
 title('(4) SCOPE - Grayscale Image'); 
+axis image;
 
+%% Demonstrate Filters
 f5 = figure(5);
 medfilter = medfilt2(grid3);
-temp = medfilter(:,horiz_range);
-image(temp, 'CDataMapping','scaled');
-title('SCOPE - Median Filterd');
+image(medfilter, 'CDataMapping','scaled');
+title('SCOPE - Median Filtered');
 axis image;
 
 f6 = figure(6);
 kernel = (1/16) .* [ 1 2 1; 2 4 2; 1 2 1];
 gaussian = conv2(grid3, kernel);
-temp = gaussian(:,horiz_range);
-image(temp, 'CDataMapping','scaled');
+image(gaussian, 'CDataMapping','scaled');
 title('SCOPE - Gaussian Filterd');
 axis image;
-
-
-%% TO DO
-% - Research other interpolation methods other than weighed-average
-% - Start experimenting with performance/optimization?
